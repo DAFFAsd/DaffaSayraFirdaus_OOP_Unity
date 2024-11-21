e3c0b3ff546eee4a04bc3bc4af1ac7e96289fb24
DAFFAsd <daffa.sayra.firdaus@gmail.com>
2024-11-21 12:44:01 +0700

    CS10 - Ambatubas

--- Assets/Scripts/Weapons/Bullet.cs
+++ Assets/Scripts/Weapons/Bullet.cs
@@ -44,6 +44,10 @@ public class Bullet : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (other.CompareTag(gameObject.tag))
+        {
+            return;
+        }
         Deactivate();
     }
 }
